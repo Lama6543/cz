133e192d60bfa7b958b451e8b1593ad8e6e46d1c
Lama6543 <Lama6543@gmail.com>
2015-01-31 13:58:46 +0100

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -1581,30 +1581,30 @@
                 }
             },
 
-            vyplynovatCommand: {
-                command: 'vyplynovat',
+            cookieCommand: {
+                command: 'cookie',
                 rank: 'user',
                 type: 'startsWith',
-                cookies: ['has given you a chocolate chip cookie!',
-                    'has given you a soft homemade oatmeal cookie!',
-                    'has given you a plain, dry, old cookie. It was the last one in the bag. Gross.',
-                    'gives you a sugar cookie. What, no frosting and sprinkles? 0/10 would not touch.',
-                    'gives you a chocolate chip cookie. Oh wait, those are raisins. Bleck!',
-                    'gives you an enormous cookie. Poking it gives you more cookies. Weird.',
-                    'gives you a fortune cookie. It reads "Why aren\'t you working on any projects?"',
-                    'gives you a fortune cookie. It reads "Give that special someone a compliment"',
-                    'gives you a fortune cookie. It reads "Take a risk!"',
-                    'gives you a fortune cookie. It reads "Go outside."',
-                    'gives you a fortune cookie. It reads "Don\'t forget to eat your veggies!"',
-                    'gives you a fortune cookie. It reads "Do you even lift?"',
-                    'gives you a fortune cookie. It reads "m808 pls"',
-                    'gives you a fortune cookie. It reads "If you move your hips, you\'ll get all the ladies."',
-                    'gives you a fortune cookie. It reads "I love you."',
-                    'gives you a Golden Cookie. You can\'t eat it because it is made of gold. Dammit.',
-                    'gives you an Oreo cookie with a glass of milk!',
-                    'gives you a rainbow cookie made with love :heart:',
-                    'gives you an old cookie that was left out in the rain, it\'s moldy.',
-                    'bakes you fresh cookies, it smells amazing.'
+                cookies: ['ti dal čokoládovou šušenku!',
+                    'ti dal domácí zeleninovou sušenku!',
+                    'ti dal smradlavou, suchou a starou susšnku. Zbyla mu v batohu. Fuj.',
+                    'ti dal sladkou sušenku. Co ? Žádná chuť jen cukr ? 0/10 to je hnus.',
+                    'ti dal čokoládovou sučenku. Počkat, to je rasistické.',
+                    'ti dal obrovskou sušenku. Která vybouchla a dala ti další susšnky.',
+                    'ti dal sušenku štěstí. Píše se tu "Proč nepracuješ na žádném projektu?"',
+                    'ti chtěl dát sušenku ale cestou jí snědl',
+                    'ti dal sušenku štěstí. Píše se tu "Zariskuj!"',
+                    'ti dal sušenku štěstí. Píše se tu "Di ven."',
+                    'ti dal sušenku štěstí. Píše se tu "Nezpomeň sníst své jídlo!"',
+                    'ti dal sušenku štěstí. Píše se tu "Do you even lift?"',
+                    'ti dal sušenku štěstí. Píše se tu, počkat sušenka byla prázdná :( ',
+                    'ti dal sušenku štěstí. Píše se tu "Hýbej bokama a budeš mít všechny ženský."',
+                    'ti dal sušenku štěstí. Píše se tu "Miluji tě :heart: ."',
+                    'ti dal zlatou sušenku. Nemůžeš jí sníst protože je ze zlata. Sakra!',
+                    'ti dal sušenku oreo s sklenicí mléka!',
+                    'ti dal duhovou sušenku, vyrobenou z lásky :heart:',
+                    'ti dal starou sušenku která zůstala na dešti, je mokrá.',
+                    'ti upekl čerstvé sušenky, voní nádherně.'
                 ],
                 getCookie: function () {
                     var c = Math.floor(Math.random() * this.cookies.length);
@@ -2918,6 +2918,63 @@
                 }
             },
 
+cookieCommand: {
+                command: 'cookie',
+                rank: 'user',
+                type: 'startsWith',
+                cookies: ['ti dal čokoládovou šušenku!',
+                    'ti dal domácí zeleninovou sušenku!',
+                    'ti dal smradlavou, suchou a starou susšnku. Zbyla mu v batohu. Fuj.',
+                    'ti dal sladkou sušenku. Co ? Žádná chuť jen cukr ? 0/10 to je hnus.',
+                    'ti dal čokoládovou sučenku. Počkat, to je rasistické.',
+                    'ti dal obrovskou sušenku. Která vybouchla a dala ti další susšnky.',
+                    'ti dal sušenku štěstí. Píše se tu "Proč nepracuješ na žádném projektu?"',
+                    'ti chtěl dát sušenku ale cestou jí snědl',
+                    'ti dal sušenku štěstí. Píše se tu "Zariskuj!"',
+                    'ti dal sušenku štěstí. Píše se tu "Di ven."',
+                    'ti dal sušenku štěstí. Píše se tu "Nezpomeň sníst své jídlo!"',
+                    'ti dal sušenku štěstí. Píše se tu "Do you even lift?"',
+                    'ti dal sušenku štěstí. Píše se tu, počkat sušenka byla prázdná :( ',
+                    'ti dal sušenku štěstí. Píše se tu "Hýbej bokama a budeš mít všechny ženský."',
+                    'ti dal sušenku štěstí. Píše se tu "Miluji tě :heart: ."',
+                    'ti dal zlatou sušenku. Nemůžeš jí sníst protože je ze zlata. Sakra!',
+                    'ti dal sušenku oreo s sklenicí mléka!',
+                    'ti dal duhovou sušenku, vyrobenou z lásky :heart:',
+                    'ti dal starou sušenku která zůstala na dešti, je mokrá.',
+                    'ti upekl čerstvé sušenky, voní nádherně.'
+                ],
+                getCookie: function () {
+                    var c = Math.floor(Math.random() * this.cookies.length);
+                    return this.cookies[c];
+                },
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        var msg = chat.message;
+
+                        var space = msg.indexOf(' ');
+                        if (space === -1) {
+                            API.sendChat(basicBot.chat.eatcookie);
+                            return false;
+                        }
+                        else {
+                            var name = msg.substring(space + 2);
+                            var user = basicBot.userUtilities.lookupUserName(name);
+                            if (user === false || !user.inRoom) {
+                                return API.sendChat(subChat(basicBot.chat.nousercookie, {name: name}));
+                            }
+                            else if (user.username === chat.un) {
+                                return API.sendChat(subChat(basicBot.chat.selfcookie, {name: name}));
+                            }
+                            else {
+                                return API.sendChat(subChat(basicBot.chat.cookie, {nameto: user.username, namefrom: chat.un, cookie: this.getCookie()}));
+                            }
+                        }
+                    }
+                }
+            },
+
             websiteCommand: {
                 command: 'website',
                 rank: 'user',
@@ -2928,6 +2985,63 @@
                     else {
                         if (typeof basicBot.settings.website === "string")
                             API.sendChat(subChat(basicBot.chat.website, {link: basicBot.settings.website}));
+                    }
+                }
+            },
+
+vyplynovatCommand: {
+                command: 'vyplynovat',
+                rank: 'user',
+                type: 'startsWith',
+                cookies: ['test',
+                    'test',
+                    'test',
+                    'test',
+                    'test.',
+                    'test',
+                    'test"',
+                    'test',
+                    'test"',
+                    'test"',
+                    'test',
+                    'test',
+                    'test',
+                    'test',
+                    'test',
+                    'test',
+                    'test',
+                    'test',
+                    'test',
+                    'test'
+                ],
+                getCookie: function () {
+                    var c = Math.floor(Math.random() * this.cookies.length);
+                    return this.cookies[c];
+                },
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        var msg = chat.message;
+
+                        var space = msg.indexOf(' ');
+                        if (space === -1) {
+                            API.sendChat(basicBot.chat.eatcookie);
+                            return false;
+                        }
+                        else {
+                            var name = msg.substring(space + 2);
+                            var user = basicBot.userUtilities.lookupUserName(name);
+                            if (user === false || !user.inRoom) {
+                                return API.sendChat(subChat(basicBot.chat.nousercookie, {name: name}));
+                            }
+                            else if (user.username === chat.un) {
+                                return API.sendChat(subChat(basicBot.chat.selfcookie, {name: name}));
+                            }
+                            else {
+                                return API.sendChat(subChat(basicBot.chat.cookie, {nameto: user.username, namefrom: chat.un, cookie: this.getCookie()}));
+                            }
+                        }
                     }
                 }
             },
